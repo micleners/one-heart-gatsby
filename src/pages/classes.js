66a85cf4a797ef9ce8b4100fa0f1861de3c06d65
micleners <graindesk@Sources-MBP.domain>
2020-03-06 08:57:13 -0600

    finish classes

--- src/pages/classes.js
+++ src/pages/classes.js
@@ -1,25 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Text, Heading } from "rebass"
+import { Flex, Text, Heading } from "rebass"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const example1 = ({ data }) => {
-  console.log(data.contentfulSession)
-  console.log(data.contentfulSession.startDate)
-  console.log(data.contentfulSession.endDate)
-  console.log(data.contentfulSession.classDays)
   return (
     <Layout>
       <SEO title="Week 1" />
-      <Text>Session Start Date: {data.contentfulSession.startDate}</Text>
-      <Text>Session End Date: {data.contentfulSession.endDate}</Text>
-      <Heading py={3}>
-        Classes Offered
-      </Heading>
-      {data.contentfulSession.classDays.map(offering => (
-        <Text>{offering.name}, </Text>
+      <Text>Session Start Date: {data.contentfulSessions.startDate}</Text>
+      <Text>Session End Date: {data.contentfulSessions.endDate}</Text>
+      <Heading py={3}>Classes Offered</Heading>
+      {data.contentfulSessions.classDays.map(offering => (
+        <Flex py={3} key={offering.name} flexDirection="column">
+          <Text>{offering.name}, </Text>
+          <Text>{offering.startTime}, </Text>
+          <Text>{offering.endTime}, </Text>
+        </Flex>
       ))}
       <Link to="/">Go back to the homepage</Link>
     </Layout>
@@ -29,14 +27,14 @@ const example1 = ({ data }) => {
 export default example1
 export const pageQuery = graphql`
   query {
-    contentfulSession(week: { eq: 1 }) {
+    contentfulSessions(week: { eq: 1 }) {
       startDate
       endDate
       week
       classDays {
         day
-        endTime
-        startTime
+        endTime(formatString: "hh:mma")
+        startTime(formatString: "hh:mma")
         name
         contentful_id
       }
